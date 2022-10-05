cb80fc4d758c20219773d42f847905ec330fcae4
김수민 <106305465+ekzm9354@users.noreply.github.com>
2022-10-05 14:17:40 +0900

    카카오 로그인 시 자동회원가입

--- SoundOfMind/src/main/java/com/SoundOfMind/mapper/MemberMapper.java
+++ SoundOfMind/src/main/java/com/SoundOfMind/mapper/MemberMapper.java
@@ -15,4 +15,8 @@ public interface MemberMapper {
 	public int delete(Member member);
 	//아이디 중복체크
 	public int idCheck(String id);
+	//소셜 회원가입
+	public int SocialJoin(String root,String id);
+    //소셜 회원가입 중복체크
+	public String SocialJoinCheck(String id);
 }
