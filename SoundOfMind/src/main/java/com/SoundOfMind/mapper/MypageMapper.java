91f81d66425d951abcac5af6d8775fcdb5673fce
smhrd <smhrd@DESKTOP-EOBAMAG>
2022-10-12 11:53:50 +0900

    mypage전체

--- SoundOfMind/src/main/java/com/SoundOfMind/mapper/MypageMapper.java
+++ SoundOfMind/src/main/java/com/SoundOfMind/mapper/MypageMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.SoundOfMind.domain.Emotion;
 import com.SoundOfMind.domain.Member;
+import com.SoundOfMind.domain.Storege;
 
 @Mapper
 public interface MypageMapper {
@@ -14,5 +15,7 @@ public interface MypageMapper {
 	public String profile_save(String id);
 	
 	public int profile_update(String profile, String id);
+	
+	public List<Storege> mycomushow(String id);
 	
 }
